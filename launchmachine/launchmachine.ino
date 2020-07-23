2cb9951eebc1de91e2a622bb49d68daa6681f6d1
kakl <kakl@astro.cz>
2020-07-23 20:52:44 +0200

    launch test version

--- launchmachine/launchmachine.ino
+++ launchmachine/launchmachine.ino
@@ -2,10 +2,30 @@
 
 #define SICHER 5
 
-#define IN 3
-#define MAGNET 12
+#define IN 3      // PD3
+#define MAGNET 12 // PB4
+#define TIMEOUT 3000 // Magnet switch-on time
 
-void setup() {
+void manual_launch()
+{
+  if (!(PIND & 0b00010000)) 
+  {
+    delay(2000);
+    if (!(PIND & 0b00010000)) 
+    {
+      Serial.println("on");
+          
+      digitalWrite(MAGNET,HIGH);
+      delay(TIMEOUT);
+      digitalWrite(MAGNET,LOW);
+      delay(1000);
+    }
+  }
+ 
+}
+
+void setup() 
+{
 
   Serial.begin(9600);
 
@@ -19,14 +39,15 @@ void setup() {
 int8_t flag;
 boolean flag2 = HIGH;
 
-void loop() {
+void loop() 
+{
   flag=0;
-  while(!(PIND & 0b00001000));
-  while((PIND & 0b00001000));
+  while(!(PIND & 0b00001000)) manual_launch();
+  while((PIND & 0b00001000)) manual_launch();
   for (int8_t n=0; n<SICHER; n++)
   {
-    while((PIND & 0b00001000));
-    while(!(PIND & 0b00001000));
+    while((PIND & 0b00001000)) manual_launch();
+    while(!(PIND & 0b00001000)) manual_launch();
     delayMicroseconds(1800);
     if ((PIND & 0b00001000))
     { 
@@ -40,7 +61,7 @@ void loop() {
     Serial.println("on");
         
     digitalWrite(MAGNET,HIGH);
-    delay(1000);
+    delay(TIMEOUT);
     digitalWrite(MAGNET,LOW);
     flag2 = HIGH;
   }
@@ -49,5 +70,4 @@ void loop() {
     flag2 = LOW;
     Serial.println("off");
   }
-
 }
